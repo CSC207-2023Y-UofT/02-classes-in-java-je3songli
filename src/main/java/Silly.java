a29c3a1e4473f3438b47257863dc21b6046a7d27
je3songli <58784080+je3songli@users.noreply.github.com>
2023-05-18 10:13:27 -0400

    Completed Task 4

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -172,6 +172,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return this.name.equals(other.name);
     }
 
     /**
